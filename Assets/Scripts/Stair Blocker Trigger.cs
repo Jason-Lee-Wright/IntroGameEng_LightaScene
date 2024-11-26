20031a37ce585e916557838eb2fa6b52828162a0
Jason <jason974448@gmail.com>
2024-11-26 12:22:17 -0400

    Initial commit

---
+++ Assets/Scripts/Stair Blocker Trigger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class StairBlockerTrigger : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    bool has_triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!has_triggered)
+        {
+            timeline.Play();
+            has_triggered = true;
+        }
+        else
+        {
+            return;
+        }
+
+    }
+}
